72c26285fc58878d71bcd6d4e8c66d913d2fd504
Paras@2599 <108333711+Rise751@users.noreply.github.com>
2024-04-12 10:21:51 +0530

    delete all code

--- src/contracts/script/deploy.anvil.sh
+++
@@ -1,4 +0,0 @@
-# source .env
-forge script script/deploy.anvil.s.sol:DeployAnvil \
-    -f http://127.0.0.1:8545 \
-    --broadcast
--- src/state/lists/actions.ts
+++
@@ -1,4 +0,0 @@
-import { createAction } from '@reduxjs/toolkit'
-import { TokenList } from 'dxswap-sdk'
-
-export const setTokenList = createAction<TokenList>('setTokenList')
